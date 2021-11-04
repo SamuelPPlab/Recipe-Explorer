c3ee8aec4477e29e8e62d04edae9923bd553d57b
SamuelPPlab <samuelpp.s@hotmail.com>
2021-11-04 11:30:31 -0300

    share button refatorado

--- src/components/CopyLinkButton.jsx
+++ src/components/CopyLinkButton.jsx
@@ -5,12 +5,14 @@ import Button from "./Button";
 
 const CopyLinkButton = ({ pathname }) => {
   const share = <img src={shareIcon} alt="share icon" style={{ width: '50px' }} />;
+  const shareButtonProps = {
+    name: share,
+    id: "share-btn",
+    onClick: () => copyLink(pathname),
+  };
+
   return(
-    <Button
-      name={share}
-      id="share-btn"
-      onClick={() => copyLink(pathname)}
-    />
+    <Button {...shareButtonProps} />
   )
 }
 
