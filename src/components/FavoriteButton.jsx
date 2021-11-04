c65a144f5d0ba41dfc721d4a57743c18d3e3f9cb
SamuelPPlab <samuelpp.s@hotmail.com>
2021-11-04 11:50:55 -0300

    favorite button refatorado

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -9,17 +9,21 @@ const FavoriteButton = ({ id }) => {
   const favorite = currentRecipe[id] ? currentRecipe[id].favorite : false;
   const whiteHeart = <img src={whiteHeartIcon} alt="white heart" style={{ width: '50px'}} />;
   const blackHeart = <img src={blackHeartIcon} alt="black heart" style={{ width: '50px'}} />;
+
   const [fav, setFav] = useState(favorite);
+
+  const favButtonProps = {
+    type: "button",
+    id: "favorite",
+    name: fav ? blackHeart : whiteHeart,
+    onClick: () => {
+      setFavorite(id, favorite);
+      setFav(!fav);
+    },
+  };
+
   return (
-    <Button
-      type="button"
-      id="favorite"
-      name={fav ? blackHeart : whiteHeart}
-      onClick={ () => {
-        setFavorite(id, favorite);
-        setFav(!fav);
-      } }
-    />
+    <Button {...favButtonProps} />
   )
 }
 
