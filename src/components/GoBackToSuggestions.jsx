f2163de55d33bd0f434009ce2120a679e591324c
SamuelPPlab <samuelpp.s@hotmail.com>
2021-11-04 07:17:18 -0300

    botão voltar para tela principal adicionado na tela de sugestoes

--- src/components/GoBackToSuggestions.jsx
+++ src/components/GoBackToSuggestions.jsx
@@ -5,7 +5,7 @@ import suggestionIcon from '../images/suggestionIcon.png';
 const GoBackToSuggestions = () => {
   const [redirect, setRedirect] = useState(false);
 
-  if(redirect) return <Redirect to='/suggestions' />
+  if(redirect) return <Redirect to='/suggestions' />;
   const suggestionIMG = <img src={suggestionIcon} alt='suggetion icon' style={{ width: '50px' }} />;
   return(
     <div>
