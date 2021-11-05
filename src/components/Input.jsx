7647b4a6f7cf652fd699239ad214b49b6d948cd8
SamuelPPlab <samuelpp.s@hotmail.com>
2021-11-05 11:28:18 -0300

    componente Input refatorado

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,19 +3,21 @@ import PropTypes from 'prop-types';
 
 function Input({ name, fieldValue = null, checked = null,
   setFieldValue = null, type = 'text', readOnly = false, style = null }) {
+  const inputProps = {
+    checked,
+    value: fieldValue,
+    id: name,
+    onChange: ({ target: { value } }) => setFieldValue(value),
+    type: type,
+    readOnly: readOnly,
+  };
+
   return (
     <div>
       <label htmlFor={ name } style={style}>
         {name}
         <div>
-          <input
-            checked={ checked }
-            value={ fieldValue }
-            id={ name }
-            onChange={ ({ target: { value } }) => setFieldValue(value) }
-            type={ type }
-            readOnly={ readOnly }
-          />
+          <input />
         </div>
     </label>
     </div>
