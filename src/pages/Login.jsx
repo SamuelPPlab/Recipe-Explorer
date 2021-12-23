2288ab84abc8d6a241a76e308ba06e0f9cfcac76
SamuelPPlab <samuelpp.s@hotmail.com>
2021-12-23 04:19:36 -0300

    Estilizaçao da tela de login quase finalizada.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import checkLogin from '../services/loginValidator';
-import Grid from "@material-ui/core/Grid";
-import FormLabel from "@material-ui/core/FormLabel";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import Radio from "@material-ui/core/Radio";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import Slider from "@material-ui/core/Slider";
 import Button from "@material-ui/core/Button";
-import { Link } from 'react-router-dom';
-import { FormControl, InputAdornment, InputLabel, TextField } from '@material-ui/core';
+import { InputAdornment, TextField } from '@material-ui/core';
 import EmailIcon from '@material-ui/icons/Email';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import { emailValidator, passwordLengthValidator } from '../services/validators';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -34,6 +29,7 @@ function Login() {
 
   const emailInputProps = {
     label: "Email",
+    margin: "normal",
     id: "explorer-email",
     onChange: ({ target: { value } }) => setEmail(value),
     type: "email",
@@ -41,10 +37,12 @@ function Login() {
     InputProps: {
       endAdornment: <InputAdornment position="end"><EmailIcon color="primary" /></InputAdornment>,
     },
+    fullWidth: true,
   };
 
   const passwordInputProps = {
     id: "password",
+    margin: "normal",
     label: "Password",
     onChange: ({ target: { value } }) => setPasswordInput(value),
     type: "password",
@@ -52,6 +50,7 @@ function Login() {
     InputProps: {
       endAdornment: <InputAdornment position="end"><VpnKeyIcon color="primary" /></InputAdornment>,
     },
+    fullWidth: true,
   };
 
   const loginButtonProps = {
@@ -59,20 +58,38 @@ function Login() {
     onClick: () => setRedirect(true),
     disabled:  isDisabled,
     color: 'primary',
-    variant: 'contained'
+    variant: 'contained',
+    size: 'large',
+    fullWidth: true,
   };
 
-  const noAccount = <pre className="noAccount">Ainda não possui cadastro? <Link to="/signup">Cadastre-se</Link></pre>;
-
   return (
-    <div>
-      <TextField {...emailInputProps} />
-      <TextField {...passwordInputProps} />
-      <Button {...loginButtonProps}>
-        Entrar
-      </Button>
+    <div style={{ height: '100vh', width: '100vw', marginLeft: '-8px', marginTop: '-8px', position: 'fixed', background: 'radial-gradient(circle, rgba(237,237,237,1) 53%, rgba(162,162,162,1) 100%)' }}>
+      <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column',
+        justifyContent: 'center' }}>
+        <div style={{ width: '80%', height: '50%', marginLeft: '10%', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+          <div style={{ width: '30vw', justifyContent: 'center' }}>
+            <h1 style={{ fontWeight: "bold", fontSize: "3em", textAlign: "center", textShadow: '0 0 5px #3f51b5' }}>
+              Seja bem-vindo!
+            </h1>
+            <TextField {...emailInputProps} />
+            <TextField {...passwordInputProps} />
+            <div style={{ width: '80%', marginTop: '4%', marginLeft: '10%', display: 'flex', justifyContent: 'center' }}>
+              <Button {...loginButtonProps}>
+                Entrar
+              </Button>
+            </div>
+            <div style={{ display: 'flex', marginTop: '2%', justifyContent: 'center' }}>
+              <p>Ainda não possui cadastro?</p>
+                <Button variant='text' color='primary' size="small" href="/signup" disableRipple disableFocusRipple>
+                  Cadastre-se!
+                </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
+ 
 export default Login;
