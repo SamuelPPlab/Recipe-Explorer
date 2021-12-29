6e5711a93abc5ed9d75dc0b80010a89b0a578b4c
SamuelPPlab <samuelpp.s@hotmail.com>
2021-12-29 15:38:12 -0300

    Tooltip adicionada no botão de visitar a tela de sugestoes.

--- src/components/GoBackToSuggestions.jsx
+++ src/components/GoBackToSuggestions.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
-import { Button } from "@material-ui/core";
+import { Button, Tooltip } from "@material-ui/core";
+
 
 const GoBackToSuggestions = () => {
   const suggestionProps = {
@@ -11,9 +12,11 @@ const GoBackToSuggestions = () => {
   };
 
   return(
-    <Button style={{ width: '50px', height: '50px' }} {...suggestionProps}>
-      <EmojiObjectsIcon fontSize="large" />
-    </Button>
+    <Tooltip title="See Suggestions for the day" placement="right">
+      <Button style={{ width: '50px', height: '50px' }} {...suggestionProps}>
+        <EmojiObjectsIcon fontSize="large" />
+      </Button>
+    </Tooltip>
   );
 }
 
